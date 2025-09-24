b7f9a8979bc81c553df5f3db7e3ed4dc13194f91
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-09-24 18:14:31 +0200

    feat: Ejercicio 3 Terminado

---
+++ EjercicioT01_Ej-3/src/App.java
@@ -0,0 +1,47 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String computer = "computer";
+        String ordenador = "ordenador";
+        String student = "student";
+        String alumno = "alumno/a";
+        String cat = "cat";
+        String gato = "gato";
+        String penguin = "penguin";
+        String pinguino = "pingüino";
+        String machine = "machine";
+        String maquina = "máquina";
+        String nature = "nature";
+        String naturaleza = "naturaleza";
+        String light = "light";
+        String luz = "luz";
+        String hot = "hot";
+        String caliente = "caliente";
+        String book = "book";
+        String libro = "libro";
+        String bunny = "bunny";
+        String conejo = "conejo";
+
+
+        System.out.print(computer+" ");
+        System.out.println(ordenador);
+        System.out.print(student+"  ");
+        System.out.println(alumno);
+        System.out.print(cat);
+        System.out.printf("%10s%n",gato);
+        System.out.print(penguin+"  ");
+        System.out.println(pinguino);
+        System.out.print(machine+"  ");
+        System.out.println(maquina);
+        System.out.print(nature+"   ");
+        System.out.println(naturaleza);
+        System.out.print(light);
+        System.out.printf("%7s%n",luz);
+        System.out.print(hot);
+        System.out.printf("%14s%n",caliente);
+        System.out.print(book);
+        System.out.printf("%10s%n",libro);
+        System.out.print(bunny);
+        System.out.printf("%10s",conejo);
+
+    }
+}
