aa1c218e6be9470fa75bb5d7a9c8520b741c6616
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-09-24 17:32:02 +0200

    fix: Ejercicio 1 Terminado

---
+++ EjercicioT01_Ej-1/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String name = "Eliel Jesus Garcia Vela";
+
+        System.out.println(name);  
+    }
+}
