d24dfb9cd6736a231ccf350a11e7c3296eba219b
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-09-24 17:45:07 +0200

    feat: Ejercicio 2 Terminado

---
+++ EjercicioT01_Ej-2/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "Eliel Jesus Garcia Vela";
+        String address = "Calle Pedro Macanaz N4, 2ºB, Campanillas (Málaga) - España";
+        int number = 12344567;
+
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(number);
+    }
+}
