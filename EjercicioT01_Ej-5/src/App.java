e462172707d0784efdd19873542a6c00be924796
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-09-25 13:33:24 +0200

    fix: optimized the schedule

---
+++ EjercicioT01_Ej-5/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("┌──────────────────────────────────────────────────┐");
+        System.out.printf("%-1s%51s%n", "│", "│");
+        System.out.printf("%-1s%7s%10s%10s%10s%10s%4s%n", "│", "Lunes", "Martes", "Miérc.", "Jueves.", "Viernes", "│");
+        System.out.println("├──────────────────────────────────────────────────┤");
+        System.out.printf("%-1s%6s%9s%9s%9s%9s%9s%n", "│", "PROG", "IPEI", "PROG", "LM", "LM", "│");
+        System.out.println("├──────────────────────────────────────────────────┤");
+        System.out.printf("%-1s%6s%9s%9s%9s%9s%9s%n", "│", "PROG", "IPEI", "PROG", "LM", "BD", "│");
+        System.out.println("├──────────────────────────────────────────────────┤");
+        System.out.printf("%-1s%6s%9s%9s%9s%9s%9s%n", "│", "SI", "IPEI", "PROG", "BD", "BD", "│");
+        System.out.println("├──────────────────────────────────────────────────┤");
+        System.out.printf("%-1s%6s%9s%9s%9s%9s%9s%n", "│", "SI", "PROG", "DIG", "BD", "SI", "│");
+        System.out.println("├──────────────────────────────────────────────────┤");
+        System.out.printf("%-1s%6s%9s%9s%9s%9s%9s%n", "│", "BD", "PROG", "ED", "SOS", "SI", "│");
+        System.out.println("├──────────────────────────────────────────────────┤");
+        System.out.printf("%-1s%6s%9s%9s%9s%9s%9s%n", "│", "BD", "PROG", "ED", "ED", "SI", "│");
+        System.out.println("└──────────────────────────────────────────────────┘");
+    }
+}
