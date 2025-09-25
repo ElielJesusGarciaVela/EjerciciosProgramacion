21097398acaa8c2106125febf44a3a2422b0d216
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-09-25 13:55:05 +0200

    feat: added exercise 9

---
+++ EjercicioT01_Ej-9/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        final String AMARILLO = "\u001B[33m";
+        final String RESET = "\u001B[0m";
+        final String BACKGROUND_RED = "\u001B[41m";
+        final String BACKGROUND_BLACK = "\u001B[40m";
+
+        System.out.println(BACKGROUND_RED+"                                                      "+RESET);
+        System.out.println(BACKGROUND_RED+"                                          "+BACKGROUND_BLACK+"   "+BACKGROUND_RED+"              "+RESET);
+        System.out.println(BACKGROUND_RED+"                                                      "+RESET);
+        System.out.println(BACKGROUND_RED+"            "+AMARILLO+"APRENDE JAVA"+"              "+BACKGROUND_BLACK+"   "+BACKGROUND_RED+"                  "+RESET);
+        System.out.println(BACKGROUND_RED+"                                                      "+RESET);
+        System.out.println(BACKGROUND_RED+"            "+AMARILLO+"CON EJERCICIOS"+"               "+BACKGROUND_BLACK+"   "+BACKGROUND_RED+"                "+RESET);
+        System.out.println(BACKGROUND_RED+"                                                      "+RESET);
+        System.out.println(BACKGROUND_RED+"          "+AMARILLO+"Eliel Jesus Garcia Vela"+"  "+BACKGROUND_BLACK+"                "+BACKGROUND_RED+"        "+RESET);
+        System.out.println(BACKGROUND_RED+"                                   "+BACKGROUND_BLACK+"          "+BACKGROUND_RED+"  "+BACKGROUND_BLACK+"    "+RESET);
+        System.out.println(BACKGROUND_RED+"                                   "+BACKGROUND_BLACK+"                    "+RESET);
+    }  
+}
