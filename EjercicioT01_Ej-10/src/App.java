db61c0d9c143f6831a0c8843562992366773d9ff
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-09-27 22:08:05 +0200

    fix: Finished exercise 9 and 10

---
+++ EjercicioT01_Ej-10/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        final String AMARILLO = "\u001B[33m";
+        final String RESET = "\u001B[0m";
+        final String BACKGROUND_RED = "\u001B[41m";
+        final String BACKGROUND_BLACK = "\u001B[40m";
+
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + BACKGROUND_BLACK + "███"+ BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒" + AMARILLO + "APRENDE JAVA" + RESET + BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒"+ BACKGROUND_BLACK + "███" + BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒" + AMARILLO + "CON EJERCICIOS" + RESET + BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"+ BACKGROUND_BLACK + "███" + BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒" + AMARILLO + "Eliel Jesus Garcia Vela" + RESET + BACKGROUND_RED + "▒▒"+ BACKGROUND_BLACK + "████████████████" + BACKGROUND_RED + "▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + BACKGROUND_BLACK+ "██████████████████" + BACKGROUND_RED + "▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + BACKGROUND_BLACK + "██████████████"+ BACKGROUND_RED + "▒▒" + BACKGROUND_BLACK + "██" + BACKGROUND_RED + "▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + BACKGROUND_BLACK + "██████████████"+ BACKGROUND_RED + "▒▒" + BACKGROUND_BLACK + "██" + BACKGROUND_RED + "▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + BACKGROUND_BLACK + "██████████████"+ BACKGROUND_RED + "▒▒" + BACKGROUND_BLACK + "██" + BACKGROUND_RED + "▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + BACKGROUND_BLACK+ "██████████████████" + BACKGROUND_RED + "▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + BACKGROUND_BLACK + "███████████████"+ BACKGROUND_RED + "▒▒▒▒▒" + RESET);
+        System.out.println(BACKGROUND_RED + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + RESET);
+    }
+}
--- EjercicioT01_Ej-9/src/App.java
+++ EjercicioT01_Ej-9/src/App.java
@@ -1,20 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        final String AMARILLO = "\u001B[33m";
-        final String RESET = "\u001B[0m";
-        final String BACKGROUND_RED = "\u001B[41m";
-        final String BACKGROUND_BLACK = "\u001B[40m";
-
-        System.out.println(BACKGROUND_RED+"                                                      "+RESET);
-        System.out.println(BACKGROUND_RED+"                                          "+BACKGROUND_BLACK+"   "+BACKGROUND_RED+"              "+RESET);
-        System.out.println(BACKGROUND_RED+"                                                      "+RESET);
-        System.out.println(BACKGROUND_RED+"            "+AMARILLO+"APRENDE JAVA"+"              "+BACKGROUND_BLACK+"   "+BACKGROUND_RED+"                  "+RESET);
-        System.out.println(BACKGROUND_RED+"                                                      "+RESET);
-        System.out.println(BACKGROUND_RED+"            "+AMARILLO+"CON EJERCICIOS"+"               "+BACKGROUND_BLACK+"   "+BACKGROUND_RED+"                "+RESET);
-        System.out.println(BACKGROUND_RED+"                                                      "+RESET);
-        System.out.println(BACKGROUND_RED+"          "+AMARILLO+"Eliel Jesus Garcia Vela"+"  "+BACKGROUND_BLACK+"                "+BACKGROUND_RED+"        "+RESET);
-        System.out.println(BACKGROUND_RED+"                                   "+BACKGROUND_BLACK+"          "+BACKGROUND_RED+"  "+BACKGROUND_BLACK+"    "+RESET);
-        System.out.println(BACKGROUND_RED+"                                   "+BACKGROUND_BLACK+"                    "+RESET);
-    }  
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "███" + "▒▒▒▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒" + "APRENDE JAVA" + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "███" + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒" + "CON EJERCICIOS" + "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "███" + "▒▒▒▒▒▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒" + "Eliel Jesus Garcia Vela" + "▒▒" + "████████████████" + "▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "██████████████████" + "▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "██████████████"+ "▒▒" + "██" + "▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "██████████████" + "▒▒" + "██" + "▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "██████████████" + "▒▒" + "██" + "▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "██████████████████" + "▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" + "███████████████" + "▒▒▒▒▒");
+        System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+    }
 }
